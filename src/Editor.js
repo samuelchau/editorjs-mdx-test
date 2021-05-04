b056fbd02da04cdaa6828dba8a39f3f10252d357
Samuel Chau <samuel.chau@mail.mcgill.ca>
2021-05-03 21:19:36 -0400

    - Added the dependencies '@editorjs/editorjs' and '@editorjs/header' - Created the React component Editor.js to display an instance of EditorJS

---
+++ src/Editor.js
@@ -0,0 +1,77 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { default as React, useEffect, useRef } from 'react';
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header'; 
+
+const DEFAULT_INITIAL_DATA = () => {
+  return {
+    "time": new Date().getTime(),
+    "blocks": [
+      {
+        "type": "header",
+        "data": {
+          "text": "This is a header.",
+          "level": 1
+        }
+      },
+    ]
+  }
+}
+
+const EDITTOR_HOLDER_ID = 'editorjs';
+
+const Editor = (props) => {
+  const ejInstance = useRef();
+  const [editorData, setEditorData] = React.useState(DEFAULT_INITIAL_DATA);
+  const [output, setOutput] = React.useState(JSON.stringify(DEFAULT_INITIAL_DATA, null, 4));
+
+  // This will run only once
+  useEffect(() => {
+    if (!ejInstance.current) {
+      initEditor();
+    }
+    return () => {
+      ejInstance.current.destroy();
+      ejInstance.current = null;
+    }
+  }, []);
+
+  const initEditor = () => {
+    const editor = new EditorJS({
+      holder: EDITTOR_HOLDER_ID,
+      logLevel: "ERROR",
+      data: editorData,
+      onReady: () => {
+        ejInstance.current = editor;
+      },
+      onChange: async () => {
+        console.log("onChange()");
+        let content = await this.editorjs.saver.save();
+        // Put your logic here to save this data to your DB
+        setEditorData(content);
+      },
+      autofocus: true,
+      tools: { 
+        header: Header, 
+      }, 
+    });
+  };
+
+  const saveEditorContent = () => {
+    ejInstance.current?.save().then( savedData => {
+      console.log("save content... ", JSON.stringify(savedData, null, 4));
+      setOutput(JSON.stringify(savedData, null, 4));
+    })
+    
+  }
+
+  return (
+    <React.Fragment>
+      <button id="save-button" onClick={saveEditorContent}>Click to display output</button>
+      <div style={{background: "#cceeee"}} id="output">{output}</div>
+      <div id={EDITTOR_HOLDER_ID}> </div>
+    </React.Fragment>
+  );
+}
+
+export default Editor;
