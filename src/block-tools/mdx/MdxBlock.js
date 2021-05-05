a88804e23f2adc622818efed1ef3c53a46ecbfd7
Samuel Chau <samuel.chau@mail.mcgill.ca>
2021-05-04 21:43:37 -0400

    - Added new dependency 'react-charts'
    - Added new custom component 'MyChart' for use in the MDX content

--- src/block-tools/mdx/MdxBlock.js
+++ src/block-tools/mdx/MdxBlock.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { CustomButton, CustomCounter, CustomSelectionBox } from './components/CustomComponents';
+import { CustomButton, CustomCounter, CustomSelectionBox, MyChart } from './components/CustomComponents';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -39,6 +39,7 @@ const MdxBlock = (props) => {
     MyButton: props => <CustomButton {...props}/>,
     MyCounter: props => <CustomCounter {...props}/>,
     MyFruitSelection: props => <CustomSelectionBox {...props}/>,
+    MyChart: () => <MyChart/>,
   }
 
   return (
--- src/block-tools/mdx/components/CustomComponents.js
+++ src/block-tools/mdx/components/CustomComponents.js
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-
+import { useState, useMemo } from 'react'
+import { Chart } from 'react-charts'
 
 export const CustomButton = () => (
   <div style={{padding: 20, backgroundColor: 'red'}} >
@@ -35,10 +35,47 @@ export const CustomSelectionBox = (props) => {
 
   return (
     <select value={selected} onChange={handleChange} multiple>
-      {
-        options.map( (item, index)=> <option key={`${item}-${index}`} value={item} >{item}</option> )
-      }
+    {
+      options.map( (item, index)=> <option key={`${item}-${index}`} value={item} >{item}</option> )
+    }
     </select>
+  )
+}
+
+
+// A simple line chart example taken from https://www.npmjs.com/package/react-charts
+export function MyChart() {
+  const data = useMemo(
+    () => [
+      {
+        label: 'Series 1',
+        data: [[0, 1], [1, 2], [2, 4], [3, 2], [4, 7]]
+      },
+      {
+        label: 'Series 2',
+        data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
+      }
+    ],
+    []
+  )
+
+  const axes = useMemo(
+    () => [
+      { primary: true, type: 'linear', position: 'bottom' },
+      { type: 'linear', position: 'left' }
+    ],
+    []
+  )
+
+  return (
+    <div
+      style={{
+        width: '400px',
+        height: '300px'
+      }}
+    >
+      <Chart data={data} axes={axes} tooltip />
+    </div>
   )
 }
 
